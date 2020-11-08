4de4ac2676a82ffa49a50adb7a372090a11eaa2b
alex <alex@tekartik.com>
2020-11-08 12:56:26 +0100

    feat: pseudo mixin export

---
+++ dev_test/lib/src/mixin/package_io.dart
@@ -0,0 +1,7 @@
+/// Pseudo exports.
+///
+/// Used internally.
+export 'package:dev_test/src/package/package_io_impl.dart'
+    show pathGetPubspecYamlMap, pathGetAnalysisOptionsYamlMap, pathGetYaml;
+export 'package:dev_test/src/package/recursive_pub_path.dart'
+    show recursiveActions, recursivePubPath;
