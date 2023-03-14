2d5d8e6f7ba4303b76e0da94ec19fbe931092699
Alexandre Roux <alex@tekartik.com>
2023-03-14 01:06:24 +0100

    fix: webdev activation

--- dev_test/test/pub_global_test.dart
+++ dev_test/test/pub_global_test.dart
@@ -7,13 +7,17 @@ import 'package:test/test.dart';
 
 Future<void> main() async {
   group('pub global', () {
-    test('checkAndActivateWebdev verbose', () async {
+    test('deactivate then checkAndActivateWebdev', () async {
       if (await isPackageActivated('webdev', verbose: true)) {
         await deactivatePackage('webdev', verbose: true);
       }
       await checkAndActivateWebdev(verbose: true);
       await run('dart pub global run webdev --version');
     }, timeout: const Timeout(Duration(minutes: 5)));
+    test('checkAndActivateWebdev verbose', () async {
+      await checkAndActivateWebdev(verbose: true);
+      await run('dart pub global run webdev --version');
+    });
     test('checkAndActivateWebdev silent', () async {
       await checkAndActivateWebdev();
     });
