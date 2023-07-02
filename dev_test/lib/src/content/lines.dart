be07b4be703fb2d2a3afbfa9ecb1f34e6ea1b4b2
Alexandre Roux <alex@tekartik.com>
2023-07-02 17:50:06 +0200

    [dev_test] feat: allow setting any key in a given yaml file

--- dev_test/lib/src/content/lines.dart
+++ dev_test/lib/src/content/lines.dart
@@ -4,6 +4,40 @@ import 'package:yaml/yaml.dart';
 
 import 'characters.dart';
 
+const _backtick = 0x60;
+
+/// Unescape a table or column name.
+String unescapeKeyName(String name) {
+  final codeUnits = name.codeUnits;
+  if (_areCodeUnitsEscaped(codeUnits)) {
+    return name.substring(1, name.length - 1);
+  }
+  return name;
+}
+
+// escape with back tick
+String escapeKeyName(String name) {
+  return '`$name`';
+}
+
+bool isKeyNameEscaped(String name) {
+  final codeUnits = name.codeUnits;
+  return _areCodeUnitsEscaped(codeUnits);
+}
+
+/// True if already escaped
+bool _areCodeUnitsEscaped(List<int> codeUnits) {
+  if (codeUnits.isNotEmpty) {
+    final first = codeUnits.first;
+    switch (first) {
+      case _backtick:
+        final last = codeUnits.last;
+        return last == first;
+    }
+  }
+  return false;
+}
+
 /// YamlLinesContent
 abstract class YamlLinesContent {
   /// Split lines.
@@ -58,7 +92,56 @@ extension YamlLinesContentExtension on YamlLinesContent {
 
   /// Returns true if modified.
   bool _setOrAppendKey(String key, String value) {
-    var index = indexOfTopLevelKey(key);
+    String topLovelKey;
+    if (isKeyNameEscaped(key)) {
+      topLovelKey = unescapeKeyName(key);
+    } else {
+      var keyPaths = key.split('.');
+
+      if (keyPaths.length > 1) {
+        var modified = false;
+        var lineIndex = 0;
+        for (var depth = 0; depth < keyPaths.length; depth++) {
+          var keyPart = keyPaths[depth];
+          var last = depth == keyPaths.length - 1;
+
+          String getExpectedLine() {
+            return '${_preDepth(depth)}$keyPart:${last ? ' $value' : ''}';
+          }
+
+          // Find top level key
+          var index = indexOfSubLevelKey(keyPart, depth, lineIndex);
+          if (index >= 0) {
+            lineIndex = index;
+            if (lines[lineIndex] != getExpectedLine()) {
+              modified = true;
+              lines[lineIndex] = getExpectedLine();
+            }
+            lineIndex++;
+          } else {
+            // Add before next upper level key
+            var positionIndex = lineIndex;
+            while (positionIndex < lines.length) {
+              var nextDepth = _getLineDepth(lines[positionIndex]);
+              if (nextDepth != null && nextDepth < depth) {
+                lineIndex = positionIndex;
+                break;
+              } else {
+                positionIndex++;
+              }
+            }
+
+            modified = true;
+            // Create top level key
+            lines.insert(lineIndex, getExpectedLine());
+            lineIndex++;
+          }
+        }
+        return modified;
+      }
+      topLovelKey = keyPaths.first;
+    }
+    var index = indexOfTopLevelKey(topLovelKey);
     if (index >= 0) {
       var newLine = '$key: $value';
       var existingLine = lines[index];
@@ -74,6 +157,7 @@ extension YamlLinesContentExtension on YamlLinesContent {
     }
   }
 
+  /// key can contain dot.separated.keys unless escaped
   bool setOrAppendKey(String key, String value) {
     if (_setOrAppendKey(key, value)) {
       _mixin.reloadYaml();
@@ -91,6 +175,47 @@ extension YamlLinesContentExtension on YamlLinesContent {
       if (line.startsWith(key) &&
           line.substring(key.length).trim().startsWith(':')) {
         return i;
+      }
+    }
+
+    return -1;
+  }
+
+  int? _getLineDepth(String line) {
+    var count = 0;
+    for (var char in line.codeUnits) {
+      if (isWhitespace(char)) {
+        count++;
+      } else {
+        break;
+      }
+    }
+    return count ~/ 2;
+  }
+
+  String _preDepth(int depth) {
+    var sb = StringBuffer();
+    for (var i = 0; i < depth; i++) {
+      sb.write('  ');
+    }
+    return sb.toString();
+  }
+
+  int indexOfSubLevelKey(String key, int depth, int startIndex) {
+    for (var i = startIndex; i < lines.length; i++) {
+      var line = lines[i];
+      // Assume a proper format
+      var lineDepth = _getLineDepth(line);
+      if (lineDepth != null) {
+        if (lineDepth == depth) {
+          line = line.trim();
+          if (line.startsWith(key) &&
+              line.substring(key.length).trim().startsWith(':')) {
+            return i;
+          }
+        } else if (lineDepth < depth) {
+          return -1;
+        }
       }
     }
 
--- dev_test/test/multiplatform/content_test.dart
+++ dev_test/test/multiplatform/content_test.dart
@@ -39,5 +39,28 @@ void main() {
       expect((content..setOrAppendKey('v', '1')).toContent(),
           'a: 1\nv: 1\nb: 3\n');
     });
+    test('dot.support', () async {
+      var content = YamlLinesContent.withText('');
+      expect(content.setOrAppendKey('a.b', '1'), isTrue);
+      expect(content.toContent(), 'a:\n  b: 1\n');
+      expect(content.setOrAppendKey('a.b', '1'), isFalse);
+      expect(content.setOrAppendKey('a.b', '2'), isTrue);
+      expect(content.toContent(), 'a:\n  b: 2\n');
+      expect(content.setOrAppendKey('c', '3'), isTrue);
+      expect(content.toContent(), 'a:\n  b: 2\nc: 3\n');
+      expect(content.setOrAppendKey('a.d', '4'), isTrue);
+      expect(content.setOrAppendKey('c', '5'), isTrue);
+      expect(content.toContent(), 'a:\n  b: 2\n  d: 4\nc: 5\n');
+      expect(content.setOrAppendKey('a.b.d.e.f', '6'), isTrue);
+      expect(
+          content.toContent(),
+          'a:\n'
+          '  b:\n'
+          '    d:\n'
+          '      e:\n'
+          '        f: 6\n'
+          '  d: 4\n'
+          'c: 5\n');
+    });
   });
 }
