e27446526a0f7f13e562803fa1b79b9fbe1c533e
Alexandre Roux <alex@tekartik.com>
2026-06-03 12:20:43 +0200

    dev_build: 1.1.8+1

--- dev_build/lib/src/version.dart
+++ dev_build/lib/src/version.dart
@@ -4,7 +4,7 @@ library;
 import 'package:pub_semver/pub_semver.dart';
 
 /// Package version text
-const packageVersionText = '1.1.8';
+const packageVersionText = '1.1.8+1';
 
 /// Package version
 final packageVersion = Version.parse(packageVersionText);
